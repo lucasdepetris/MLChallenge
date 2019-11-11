d27dabd36648e03c8867a24f9a9162e26de9f8b7
david <dhermosilla@huenei.com>
2019-11-11 09:45:33 -0300

    -Save state activity

--- app/src/main/java/com/depetris/lucas/mercadolibrechallenge/MainActivity.java
+++ app/src/main/java/com/depetris/lucas/mercadolibrechallenge/MainActivity.java
@@ -1,6 +1,7 @@
 package com.depetris.lucas.mercadolibrechallenge;
 
 import android.content.res.Resources;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -28,11 +29,23 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.ReciclerProductos)
     RecyclerView recyclerView;
 
+    private BusquedaResponse resp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        if(savedInstanceState != null){
+            this.resp = savedInstanceState.getParcelable("response");
+            if(this.resp != null && !resp.getResults().isEmpty()){
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+                recyclerView.setLayoutManager(linearLayoutManager);
+                ProductosAdaptador adapter = new ProductosAdaptador(resp.getResults());
+                recyclerView.setAdapter(adapter);
+            }
+        }
         buscador.setIconifiedByDefault(false);
         buscador.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -48,12 +61,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putParcelable("response",resp);
+        // call superclass to save any view hierarchy
+        super.onSaveInstanceState(outState);
+    }
+
     public void buscarProd(String nombre) {
         findViewById(R.id.msgError).setVisibility(View.GONE);
         findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
         HttpService.getInstance().BuscarProducto(nombre, new HttpService.CustomCallListener<BusquedaResponse>() {
             @Override
             public void getResult(BusquedaResponse response) {
+                resp = response;
                 LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
                 recyclerView.setLayoutManager(linearLayoutManager);
 
--- app/src/main/java/com/depetris/lucas/mercadolibrechallenge/Modelo/BusquedaResponse.java
+++ app/src/main/java/com/depetris/lucas/mercadolibrechallenge/Modelo/BusquedaResponse.java
@@ -1,11 +1,31 @@
 package com.depetris.lucas.mercadolibrechallenge.Modelo;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.Serializable;
 import java.util.List;
 
-public class BusquedaResponse implements Serializable {
+public class BusquedaResponse implements Parcelable {
     private String query;
     private List<Product> results;
+
+    protected BusquedaResponse(Parcel in) {
+        query = in.readString();
+        results = in.createTypedArrayList(Product.CREATOR);
+    }
+
+    public static final Creator<BusquedaResponse> CREATOR = new Creator<BusquedaResponse>() {
+        @Override
+        public BusquedaResponse createFromParcel(Parcel in) {
+            return new BusquedaResponse(in);
+        }
+
+        @Override
+        public BusquedaResponse[] newArray(int size) {
+            return new BusquedaResponse[size];
+        }
+    };
 
     public String getQuery() {
         return query;
@@ -21,5 +41,16 @@ public class BusquedaResponse implements Serializable {
 
     public void setResults(List<Product> results) {
         this.results = results;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(query);
+        dest.writeTypedList(results);
     }
 }
